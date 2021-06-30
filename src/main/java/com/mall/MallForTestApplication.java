cf3bdf3f8077a5c9f1df4b07a6336b598784a0f6
rkdals213 <rkdals213@naver.com>
2021-06-30 19:12:51 +0900

    초기 프로젝트 설정

---
+++ src/main/java/com/mall/MallForTestApplication.java
@@ -0,0 +1,13 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallForTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallForTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mall/MallForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallForTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
