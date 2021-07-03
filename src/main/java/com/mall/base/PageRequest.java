34e7ca5a88d7e0d94bef1652607ac8761cb1bccd
rkdals213 <rkdals213@naver.com>
2021-07-03 22:10:14 +0900

    feat: Select Shop With Paging (#2) Create Select Create Test Code Feat Support Classes

--- src/main/java/com/mall/base/PageRequest.java
+++ src/main/java/com/mall/base/PageRequest.java
@@ -1,15 +1,13 @@
 package com.mall.base;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 public class PageRequest {
     private Integer page = 1;
     private Integer size = 10;
