1a5fddadb7aa36c23f82a9f6c837f40e06c64a77
bee <rkddbtjd123@gmail.com>
2021-07-06 01:40:13 +0900

    test: import static Mockito

--- src/test/java/com/mall/Test_강유성.java
+++ src/test/java/com/mall/Test_강유성.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class Test_강유성 {
