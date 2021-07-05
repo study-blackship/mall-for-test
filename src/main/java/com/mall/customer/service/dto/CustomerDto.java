25ed88bee428498d264085a10a2ec5da53a49627
bee <rkddbtjd123@gmail.com>
2021-07-05 23:36:34 +0900

    feat: Add CustomerDto.java

---
+++ src/main/java/com/mall/customer/service/dto/CustomerDto.java
@@ -0,0 +1,21 @@
+package com.mall.customer.service.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+
+@Builder
+@Getter
+public class CustomerDto {
+
+    private Long id;
+
+    private final ZonedDateTime createdAt;
+
+    private String name;
+
+    private ZonedDateTime birth;
+
+    private AddressDto address;
+}
